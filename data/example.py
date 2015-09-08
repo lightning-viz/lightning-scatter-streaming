0f616b3c262cb757ad50c45124b5dacfa629db6d
freeman <the.freeman.lab@gmail.com>
2015-09-08 00:28:47 -0400

    Fix example

--- data/example.py
+++ data/example.py
@@ -3,12 +3,12 @@ from numpy import random, ceil, array
 
 lgn = Lightning()
 
-x = random.randn(100) * 15
-y = random.randn(100) * 15
+x = random.randn(100)
+y = random.randn(100)
 
 viz = lgn.scatterstreaming(x, y)
 
 for _ in range(100):
-	x = random.randn(100) * 15
-	y = random.randn(100) * 15
+	x = random.randn(100)
+	y = random.randn(100)
 	viz.append(x, y)
